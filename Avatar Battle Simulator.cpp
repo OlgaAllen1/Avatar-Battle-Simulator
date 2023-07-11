4a58cb1584d9f6ddd719eb3ba417b00fccdb11a3
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-07-10 19:51:40 -0600

    Add project files.

---
+++ Avatar Battle Simulator.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+   //Object oriented programming (OOP): problem-solving by modeling after real-world 
+	// concepts.
+	// Extensible: can create own types, classes, objectives, etc.
+	//to expand upon the language
+}
+
